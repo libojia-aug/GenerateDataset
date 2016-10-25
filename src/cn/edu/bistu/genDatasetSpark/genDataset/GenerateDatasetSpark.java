d73c0eb33e14f8e47ae1f010f2363487aee3f86a
李博嘉 <libojia@goyoo.com>
2016-10-25 20:14:34 +0800

    clean up code

--- src/cn/edu/bistu/genDatasetSpark/genDataset/GenerateDatasetSpark.java
+++ src/cn/edu/bistu/genDatasetSpark/genDataset/GenerateDatasetSpark.java
@@ -4,22 +4,13 @@ import cn.edu.bistu.genDataset.GenerateDataset;
 import cn.edu.bistu.genDataset.GenerateDatasetConfigBase;
 import cn.edu.bistu.genDataset.config.parameter;
 import cn.edu.bistu.utils.IPConvert;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-import scala.Tuple2;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author Berger_LBJ
@@ -55,22 +46,11 @@ public class GenerateDatasetSpark {
         int domainCount_h = (int) (count * config.getDomainFactor());
         int urlCount_h = (int) (count * config.getUrlFactor());
 
-        PairFunction<Tuple2<String, Tuple2<String, String>>, String, String> removebracket = new PairFunction<Tuple2<String, Tuple2<String, String>>, String, String>() {
-            /**
-             *
-             */
-            private static final long serialVersionUID = 1L;
-
-            public Tuple2<String, String> call(Tuple2<String, Tuple2<String, String>> arg0) throws Exception {
-                return new Tuple2<>(arg0._1, arg0._2._1 + parameter.SEPARATOR + arg0._2._2);
-            }
-        };
-
-        JavamergeData(sourceIp_h, sourceIp_l, sourceIpCount_h, count, slices)
-                .join(JavamergeData(destinationIp_h, destinationIp_l, destinationIpCount_h, count, slices))
-                .mapToPair(removebracket).join(JavamergeData(domain_h, domain_l, domainCount_h, count, slices))
-                .mapToPair(removebracket).join(JavamergeData(url_h, url_l, urlCount_h, count, slices))
-                .mapToPair(removebracket).values().map(new Function<String, String>() {
+        RDDAction.JavamergeData(sourceIp_h, sourceIp_l, sourceIpCount_h, count, slices)
+                .join(RDDAction.JavamergeData(destinationIp_h, destinationIp_l, destinationIpCount_h, count, slices))
+                .mapToPair(RDDAction.removebracket).join(RDDAction.JavamergeData(domain_h, domain_l, domainCount_h, count, slices))
+                .mapToPair(RDDAction.removebracket).join(RDDAction.JavamergeData(url_h, url_l, urlCount_h, count, slices))
+                .mapToPair(RDDAction.removebracket).values().map(new Function<String, String>() {
             /**
              *
              */
@@ -107,36 +87,5 @@ public class GenerateDatasetSpark {
             }
         }).saveAsTextFile(config.getOutputPath());
         System.out.println("生成" + (count / 10000) + "万条数据, 累计耗时:" + (System.currentTimeMillis() - time) + "ms");
-    }
-
-    public static JavaPairRDD<String, String> JavamergeData(JavaRDD<String> rdd_h, JavaRDD<String> rdd_l, int count_h,
-                                                            int count, int slices) {
-        return sc.parallelize(rdd_h.takeSample(false, count_h), slices)
-                .union(sc.parallelize(rdd_l.takeSample(false, count - count_h), slices))
-                .mapPartitionsWithIndex(new Function2<Integer, Iterator<String>, Iterator<String>>() {
-                    /**
-                     *
-                     */
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    public Iterator<String> call(Integer partitionId, Iterator<String> iterator) throws Exception {
-                        int i = 0;
-                        List<String> listTemp = new ArrayList<>(count);
-                        while (iterator.hasNext()) {
-                            i++;
-                            listTemp.add(String.valueOf(partitionId) + parameter.COMMA + String.valueOf(i)
-                                    + parameter.SEPARATOR + iterator.next());
-                        }
-                        return listTemp.iterator();
-                    }
-                }, true).mapToPair(new PairFunction<String, String, String>() {
-                    private static final long serialVersionUID = 1L;
-
-                    public Tuple2<String, String> call(String s) {
-                        String[] temp = s.split(parameter.SEPARATOR);
-                        return new Tuple2<>(temp[0], temp[1]);
-                    }
-                });
     }
 }
--- src/cn/edu/bistu/genDatasetSpark/genDataset/RDDAction.java
+++ src/cn/edu/bistu/genDatasetSpark/genDataset/RDDAction.java
@@ -3,14 +3,29 @@ package cn.edu.bistu.genDatasetSpark.genDataset;
 import cn.edu.bistu.genDataset.GenerateDatasetConfigBase;
 import cn.edu.bistu.genDataset.config.parameter;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 class RDDAction {
+    static PairFunction<Tuple2<String, Tuple2<String, String>>, String, String> removebracket = new PairFunction<Tuple2<String, Tuple2<String, String>>, String, String>() {
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+
+        public Tuple2<String, String> call(Tuple2<String, Tuple2<String, String>> arg0) throws Exception {
+            return new Tuple2<>(arg0._1, arg0._2._1 + parameter.SEPARATOR + arg0._2._2);
+        }
+    };
     private static SparkConf conf = new SparkConf().setAppName("generateDataset").setMaster("local");
     private static JavaSparkContext sc = new JavaSparkContext(conf);
     private static GenerateDatasetConfigBase config = new GenerateDatasetConfigBase();
@@ -42,4 +57,34 @@ class RDDAction {
         return sc.parallelize(inputDataset_hList);
     }
 
+    static JavaPairRDD<String, String> JavamergeData(JavaRDD<String> rdd_h, JavaRDD<String> rdd_l, int count_h,
+                                                     int count, int slices) {
+        return sc.parallelize(rdd_h.takeSample(false, count_h), slices)
+                .union(sc.parallelize(rdd_l.takeSample(false, count - count_h), slices))
+                .mapPartitionsWithIndex(new Function2<Integer, Iterator<String>, Iterator<String>>() {
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Iterator<String> call(Integer partitionId, Iterator<String> iterator) throws Exception {
+                        int i = 0;
+                        List<String> listTemp = new ArrayList<>(count);
+                        while (iterator.hasNext()) {
+                            i++;
+                            listTemp.add(String.valueOf(partitionId) + parameter.COMMA + String.valueOf(i)
+                                    + parameter.SEPARATOR + iterator.next());
+                        }
+                        return listTemp.iterator();
+                    }
+                }, true).mapToPair(new PairFunction<String, String, String>() {
+                    private static final long serialVersionUID = 1L;
+
+                    public Tuple2<String, String> call(String s) {
+                        String[] temp = s.split(parameter.SEPARATOR);
+                        return new Tuple2<>(temp[0], temp[1]);
+                    }
+                });
+    }
 }
